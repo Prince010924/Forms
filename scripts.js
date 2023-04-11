120e932607540ae2852005549e4ad02223114917
prince010924 <neosenoamadi84@gmail.com>
2023-04-11 10:17:20 +0200

    fixed function bug

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function getFormValues(){
-    let firstName = document.getElementsById("fname");
-    let lastName = document.getElementsById("lname");
-    let licensePlate = document.getElementsById("number plate");
+    let firstName = document.getElementById("fname");
+    let lastName = document.getElementById("lname");
+    let licensePlate = document.getElementById("number plate");
     let carName = document.getElementById("cars");
-    let Shade = document.getElementsById("shade");
+    let Shade = document.getElementById("shade");
    console.log(`
         first name: ${firstName}\n
         last name: ${lastName} \n
